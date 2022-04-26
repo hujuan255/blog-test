c6354b2984a42b5bd1bfc454e6136ca0c7c6bd3a
用户名 <hujuan@kaishengit.com>
2022-04-26 16:26:40 +0800

    update

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -21,5 +21,17 @@ export default {
   },
 };
 </script>
-<style lang='less' scoped>
+<style scoped>
+.post{
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.post p{
+  color: #444;
+  line-height: 40px;
+  margin-top:40px;
+}
+.pre{
+  white-space: pre-wrap;
+}
 </style>
